ea983b6c3823c25b7eab0297400c88c31e337b90
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2021-02-24 14:12:10 +0530

    #2 Floating Button Using Coordinator Layout Added

---
+++ app/src/main/java/com/lakshmi/myshoppingapp/CartActivity.kt
@@ -0,0 +1,11 @@
+package com.lakshmi.myshoppingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
