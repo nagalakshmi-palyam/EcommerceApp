9f5df1dc99f5498e1c5f9d6dbe7232805af90c92
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2021-02-26 14:03:01 +0530

    #5 Splash Activity Added

---
+++ app/src/main/java/com/lakshmi/myshoppingapp/Activities/activities/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.lakshmi.myshoppingapp.Activities.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.lakshmi.myshoppingapp.R
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DISPLAY_LENGTH = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed({
+            val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            this@SplashActivity.startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
+    }
+}
